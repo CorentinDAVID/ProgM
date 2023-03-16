d3819c02af4ccae7d7995b968c545f9e9b2943bc
Corentin DAVID <corentin-d62@laposte.net>
2023-03-16 09:59:53 +0100

    a

--- project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_p2p_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterP2pPlusPlugin.register(with: registry.registrar(forPlugin: "FlutterP2pPlusPlugin"))
 }
