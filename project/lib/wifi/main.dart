f0e71249f0d37f515ceb210ec8c8b8950f854886
Corentin DAVID <corentin-d62@laposte.net>
2023-04-03 11:07:25 +0200

    wifi

--- project/lib/wifi/main.dart
+++ project/lib/wifi/main.dart
@@ -1,311 +1,251 @@
-import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'dart:async';
-import '../main.dart';
+import 'package:flutter_p2p_connection/flutter_p2p_connection.dart';
 
-import 'package:flutter_p2p_plus/flutter_p2p_plus.dart';
-import 'package:flutter_p2p_plus/protos/protos.pb.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-
-class Wifi extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(const MaterialApp(
+    title: 'Wifi',
+    home: MultijoueursPage(),
+  ));
 }
 
-class _MyAppState extends State<Wifi> with WidgetsBindingObserver {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-  var _deviceAddress = "";
-  var _isConnected = false;
-  var _isHost = false;
-  var _isOpen = false;
+class MultijoueursPage extends StatefulWidget {
+  const MultijoueursPage({super.key});
 
-  P2pSocket? _socket;
-  List<WifiP2pDevice> devices = [];
-  final List<StreamSubscription> _subscriptions = [];
+  @override
+  State<MultijoueursPage> createState() => _MultijoueursPageState();
+}
+
+class _MultijoueursPageState extends State<MultijoueursPage> {
+  final _flutterP2pConnectionPlugin = FlutterP2pConnection();
+  WifiP2PInfo? wifiP2PInfo;
+
+  final TextEditingController _controller = TextEditingController();
+  String? _pseudo;
+  bool _isConnected = false;
+
+  StreamSubscription<WifiP2PInfo>? _streamWifiInfo;
+  StreamSubscription<List<DiscoveredPeers>>? _streamPeers;
 
   @override
   void initState() {
     super.initState();
-    _register();
-    WidgetsBinding.instance.addObserver(this);
+    _init();
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    FlutterP2pPlus.removeGroup();
-    for (var element in _subscriptions) {
-      element.cancel();
-    }
+    _flutterP2pConnectionPlugin.removeGroup();
+    _streamWifiInfo?.cancel();
+    _streamPeers?.cancel();
     super.dispose();
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _register();
-    } else if (state == AppLifecycleState.paused) {
-      _unregister();
-    }
+  void _init() async {
+    await _flutterP2pConnectionPlugin.initialize();
   }
 
-  void _register() async {
-    await Permission.location.request();
-
-    if (!await _checkPermission()) {
+  void _connect() async {
+    /*String name = _controller.text.trim();
+    print(name);
+    if (name.isEmpty) {
       return;
-    }
-    _subscriptions.add(FlutterP2pPlus.wifiEvents.stateChange!.listen((change) {
-      debugPrint("stateChange: ${change.isEnabled}");
-    }));
-
-    _subscriptions.add(FlutterP2pPlus.wifiEvents.connectionChange!.listen((change) {
-      setState(() {
-        _isConnected = change.networkInfo.isConnected;
-        _isHost = change.wifiP2pInfo.isGroupOwner;
-        _deviceAddress = change.wifiP2pInfo.groupOwnerAddress;
-      });
-      debugPrint("connectionChange: ${change.wifiP2pInfo.isGroupOwner}, Connected: ${change.networkInfo.isConnected}");
-    }));
-
-    _subscriptions.add(FlutterP2pPlus.wifiEvents.thisDeviceChange!.listen((change) {
-      debugPrint(
-          "deviceChange: ${change.deviceName} / ${change.deviceAddress} / ${change.primaryDeviceType} / ${change.secondaryDeviceType} ${change.isGroupOwner ? 'GO' : '-GO'}");
-    }));
-
-    _subscriptions.add(FlutterP2pPlus.wifiEvents.discoveryChange!.listen((change) {
-      debugPrint("discoveryStateChange: ${change.isDiscovering}");
-    }));
-
-    _subscriptions.add(FlutterP2pPlus.wifiEvents.peersChange!.listen((change) {
-      debugPrint("peersChange: ${change.devices.length}");
-      for (var device in change.devices) {
-        debugPrint("device: ${device.deviceName} / ${device.deviceAddress}");
+    }*/
+    _streamWifiInfo =
+        _flutterP2pConnectionPlugin.streamWifiP2PInfo().listen((event) {
+      wifiP2PInfo = event;
+      print(event);
+      if (event.isConnected) {
+        setState(() {
+          _isConnected = true;
+          Navigator.push(context, MaterialPageRoute(builder: (context) => MultijoueursPage()));
+        });
       }
-
-      setState(() {
-        devices = change.devices;
-      });
-    }));
-
-    FlutterP2pPlus.register();
-  }
-
-  void _unregister() {
-    for (var subscription in _subscriptions) {
-      subscription.cancel();
+    });
+    if (!await _flutterP2pConnectionPlugin.checkStoragePermission()) {
+      _flutterP2pConnectionPlugin.askStoragePermission();
     }
-    FlutterP2pPlus.unregister();
-  }
-
-  void _openPortAndAccept(int port) async {
-    if (!_isOpen) {
-      var socket = await FlutterP2pPlus.openHostPort(port);
-      setState(() {
-        _socket = socket;
-      });
-
-      var buffer = "";
-      socket?.inputStream.listen((data) {
-        var msg = String.fromCharCodes(data.data);
-        buffer += msg;
-        if (data.dataAvailable == 0) {
-          snackBar("Data Received from ${_isHost ? "Client" : "Host"}: $buffer");
-          socket.writeString("Successfully received: $buffer");
-          buffer = "";
+    if (!await _flutterP2pConnectionPlugin.checkLocationPermission()) {
+      _flutterP2pConnectionPlugin.askLocationPermission();
+    }
+    if (!await _flutterP2pConnectionPlugin.checkLocationEnabled()) {
+      _flutterP2pConnectionPlugin.enableLocationServices();
+    }
+    if (!await _flutterP2pConnectionPlugin.checkWifiEnabled()) {
+      _flutterP2pConnectionPlugin.enableWifiServices();
+    }
+    _flutterP2pConnectionPlugin.register();
+    _flutterP2pConnectionPlugin.discover();
+    _streamPeers = _flutterP2pConnectionPlugin.streamPeers().listen((event) {
+      print(event);
+      if (event.isNotEmpty) {
+        for (int i = 0; i < event.length; i++) {
+          _flutterP2pConnectionPlugin.connect(event[i].deviceAddress);
         }
-      });
-
-      debugPrint("_openPort done");
-      _isOpen = await FlutterP2pPlus.acceptPort(port) ?? false;
-      debugPrint("_accept done: $_isOpen");
-    }
+      }
+    });
   }
 
-  _connectToPort(int port) async {
-    var socket = await FlutterP2pPlus.connectToHost(
-      _deviceAddress,
-      port,
-      timeout: 100000,
-    );
-
+  void _setPseudo() {
     setState(() {
-      _socket = socket;
+      _pseudo = _controller.text;
+      _controller.clear();
     });
-
-    _socket?.inputStream.listen((data) {
-      var msg = utf8.decode(data.data);
-      snackBar("Received from ${_isHost ? "Host" : "Client"} $msg");
-    });
-
-    debugPrint("_connectToPort done");
-  }
-
-  Future<bool> _checkPermission() async {
-    // if (!await FlutterP2pPlus?.isLocationPermissionGranted()) {
-    //   await FlutterP2pPlus.requestLocationPermission();
-    //   return false;
-    // }
-    if (await Permission.location.status.isDenied) {
-      return false;
-    }
-    return true;
-  }
-
-  Future<bool?> _disconnect() async {
-    bool? result = await FlutterP2pPlus.removeGroup();
-    _unregister();
-    _socket = null;
-    if ((result ?? false)) _isOpen = false;
-    return result;
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 18, 77, 126),
-          automaticallyImplyLeading: false,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Image.asset(
-                  'images/ic_launcher.png',
-                  fit: BoxFit.contain,
-                  height: 32,
-                ),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 99, 71, 255),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 10, right: 20),
+              alignment: Alignment.topRight,
+              child: ElevatedButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const StartPage()),
-                  );
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const MultijoueursPage()));
                 },
-              ),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Text('Projet ProgM'))
-            ],
-          )),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ListTile(
-              title: const Text("Registration"),
-              subtitle: Text("P2P Registration"),
-              onTap: () async {
-                await FlutterP2pPlus.register();
-              },
-            ),
-            ListTile(
-              title: const Text("Connection State"),
-              subtitle: Text(_isConnected ? "Connected: ${_isHost ? "Host" : "Client"}" : "Disconnected"),
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Controller",
-                style: Theme.of(context).textTheme.headline5,
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Color.fromARGB(255, 162, 136, 255)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ))),
+                child: const Icon(Icons.close, color: Colors.white),
               ),
             ),
-            IntrinsicHeight(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ListTile(
-                      title: const Text("Discover Devices"),
-                      onTap: () async {
-                        if (!_isConnected) {
-                          await FlutterP2pPlus.discoverDevices();
-                        } else {
-                          return;
-                        }
-                      },
-                    ),
-                  ),
-                  VerticalDivider(
-                    color: Colors.grey,
-                  ),
-                  Expanded(
-                    child: ListTile(
-                      title: const Text("Stop Discover Devices"),
-                      onTap: () async {
-                        FlutterP2pPlus.stopDiscoverDevices();
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text("Open and accept data from port 8000"),
-              subtitle: _isConnected ? const Text("Active") : const Text("Disable"),
-              onTap: _isConnected && _isHost ? () => _openPortAndAccept(8000) : null,
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text("Connect to port 8000"),
-              subtitle: const Text("This is able to only Client"),
-              onTap: _isConnected && !_isHost ? () => _connectToPort(8000) : null,
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text("Send hello world"),
-              onTap: _isConnected ? () async => await _socket?.writeString("Hello World") : null,
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text("Disconnect"),
-              onTap: _isConnected ? () async => await _disconnect() : null,
-            ),
-            const Divider(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Device List",
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            ),
-            Expanded(
-              child: ListView(
-                children: devices.map((d) {
-                  return ListTile(
-                    title: Text(d.deviceName),
-                    subtitle: Text(d.deviceAddress),
-                    onTap: () async {
-                      debugPrint("${_isConnected ? "Disconnect" : "Connect"} to device: $_deviceAddress");
-                      if (_isConnected) {
-                        await FlutterP2pPlus.cancelConnect(d);
-                      } else {
-                        var result = (await FlutterP2pPlus.connect(d) ?? false);
-                        print("[connect] reault: $result");
-                        if (result) {
-                          _isConnected = true;
-                        }
-                        setState(() {});
-                      }
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
+            const SizedBox(height: 200),
+            _pseudo == null
+                ? Column(
+                    children: [
+                      Container(
+                        width: 300,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color.fromARGB(255, 136, 154, 255)),
+                        child: Column(children: [
+                          const Padding(
+                            padding: EdgeInsets.only(top: 20, bottom: 20),
+                            child: Text('nom',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                          ),
+                          TextField(
+                            controller: _controller,
+                            decoration: const InputDecoration(
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 74, 71, 255),
+                              labelText: "Pseudo",
+                              labelStyle: TextStyle(color: Colors.white),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Color.fromARGB(255, 71, 99, 255))),
+                              constraints: BoxConstraints(maxWidth: 220),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                        ]),
+                      ),
+                      const SizedBox(height: 30),
+                      Container(
+                          width: 300,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color.fromARGB(255, 20, 130, 255)),
+                          child: TextButton(
+                            onPressed: () {
+                              if (_controller.text.isEmpty) {
+                                return;
+                              } else {
+                                _setPseudo();
+                                _connect();
+                              }
+                            },
+                            child: const Text('VALIDER',
+                                style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                          ))
+                    ],
+                  )
+                : _isConnected
+                    ? //Container()
+                    Column(
+                        children: [
+                          Container(
+                            width: 300,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromARGB(255, 136, 138, 255)),
+                            child:  Column(children: const[
+                              Padding(
+                                padding: EdgeInsets.only(top: 20, bottom: 20),
+                                child: Text('Joueurs connectés : ',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              ),
+                              SizedBox(height: 20),
+                            ]),
+                          ),
+                          const SizedBox(height: 30),
+                          Container(
+                              width: 300,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Color.fromARGB(255, 20, 24, 255)),
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text('JOUER',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              )),
+                        ],
+                      )
+                    : Column(
+                        children: [
+                          Container(
+                            width: 300,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromARGB(255, 138, 136, 255)),
+                            child:  Column(children: const [
+                              Padding(
+                                padding: EdgeInsets.only(top: 20, bottom: 20),
+                                child: Text('En attente des autres joueurs...',
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                              ),
+                              SizedBox(height: 10),
+                              CircularProgressIndicator(),
+                              SizedBox(height: 20),
+                            ]),
+                          ),
+                        ],
+                      ),
           ],
         ),
-      ),
-    );
-  }
-
-  snackBar(String text) {
-    ScaffoldMessenger.of(_scaffoldKey.currentContext!).showSnackBar(
-      SnackBar(
-        content: Text(text),
-        duration: const Duration(seconds: 2),
       ),
     );
   }
